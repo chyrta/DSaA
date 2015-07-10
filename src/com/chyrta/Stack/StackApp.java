f796bf73ab8a096967cb415a43515bf273db4eb0
Chyrta <dima@chyrta.com>
2015-07-10 04:21:31 +0300

    Created stack and stack app

---
+++ src/com/chyrta/Stack/StackApp.java
@@ -0,0 +1,21 @@
+package com.chyrta.Stack;
+
+public class StackApp {
+
+    public static void main(String[] args) {
+
+        Stack freshStack = new Stack(10);
+        freshStack.push(20);
+        freshStack.push(40);
+        freshStack.push(60);
+        freshStack.push(80);
+
+        while (!freshStack.isEmpty()) {
+            long value = freshStack.pop();
+            System.out.print(value);
+            System.out.print(" ");
+        }
+        System.out.print("");
+    }
+
+}
